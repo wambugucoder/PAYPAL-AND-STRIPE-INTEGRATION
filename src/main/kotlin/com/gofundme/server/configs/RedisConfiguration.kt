b10c55ff8f3c9ffb849288c84079ae15559c4f0e
joswambugu <josphatwambugu77@gmail.com>
2021-02-22 12:11:33 +0300

    Scheduling Tasks

--- src/main/kotlin/com/gofundme/server/configs/RedisConfiguration.kt
+++ src/main/kotlin/com/gofundme/server/configs/RedisConfiguration.kt
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
+
+
+
 
 
 
@@ -13,13 +17,7 @@ class RedisConfiguration {
 
     @Bean
     fun jedisConnectionFactory(): JedisConnectionFactory? {
-        return JedisConnectionFactory()
-    }
-
-    @Bean
-    fun redisTemplate(): RedisTemplate<String, Any>? {
-        val template = RedisTemplate<String, Any>()
-        template.setConnectionFactory(jedisConnectionFactory()!!)
-        return template
+        val redisStandaloneConfiguration = RedisStandaloneConfiguration("localhost", 6379)
+        return JedisConnectionFactory(redisStandaloneConfiguration)
     }
 }
