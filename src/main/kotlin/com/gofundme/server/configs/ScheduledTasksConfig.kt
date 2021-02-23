e5784d3f88c6441df967b51cf29b06550e4a6434
joswambugu <josphatwambugu77@gmail.com>
2021-02-23 08:13:47 +0300

    Scheduling Operations

--- src/main/kotlin/com/gofundme/server/configs/ScheduledTasksConfig.kt
+++ src/main/kotlin/com/gofundme/server/configs/ScheduledTasksConfig.kt
@@ -3,6 +3,7 @@ package com.gofundme.server.configs
 import LogStreamResponse
 import com.gofundme.server.service.LogStream
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.scheduling.annotation.Async
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
@@ -14,6 +15,13 @@ class ScheduledTasksConfig {
     @Autowired
     lateinit var logStream: LogStream
 
+    @Async
+    // at 12:00 AM every day
+    @Scheduled(cron = "0 0 0 * * *",zone = "Africa/Nairobi")
+    fun sendApprovalEmails(){
+
+
+    }
 
 }
 
