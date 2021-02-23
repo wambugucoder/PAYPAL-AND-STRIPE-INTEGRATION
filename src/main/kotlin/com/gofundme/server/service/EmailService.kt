5a4bf4b553e2a2b4904d3ec0681a50647f23adf2
joswambugu <josphatwambugu77@gmail.com>
2021-02-23 08:48:40 +0300

    Scheduling Operations

--- src/main/kotlin/com/gofundme/server/service/EmailService.kt
+++ src/main/kotlin/com/gofundme/server/service/EmailService.kt
@@ -1,6 +1,7 @@
 package com.gofundme.server.service
 
 import LogStreamResponse
+import com.gofundme.server.model.DonationsModel
 import com.gofundme.server.requestHandler.RegisterHandler
 import io.rocketbase.commons.colors.ColorPalette
 import io.rocketbase.commons.email.EmailTemplateBuilder
@@ -21,6 +22,7 @@ class EmailService {
 
     @Autowired
     lateinit var logStream: LogStream
+
 
     fun sendAccountActivationEmail(token: String?,registerHandler: RegisterHandler) {
         val builder = EmailTemplateBuilder.builder()
@@ -76,7 +78,62 @@ class EmailService {
         catch (e:MailException){
             logStream.sendToLogConsole(LogStreamResponse(level ="ERROR",serviceAffected = "EmailService",message = "$e" ))
         }
-        logStream.sendToLogConsole(LogStreamResponse(level ="INFO",serviceAffected = "EmailService",message = "An Account Activation Email has been sent to ${registerHandler.email}" ))
+
+    }
+    fun sendAprrovalEmail(donationsModel: DonationsModel){
+            val builder = EmailTemplateBuilder.builder()
+            val header = "Donation Request Aprroval"
+
+            val htmlTextEmail: HtmlTextEmail = builder
+                .header(header)
+                .and()
+                .logo("https://www.rocketbase.io/img/logo-dark.png","",0,41)
+                .and()
+                .addText("Hello, ${donationsModel.createdBy.username}!").fontWeight(FontWeight.BOLD).center()
+                .and()
+                .addText("We are pleased that you recently created a donation with us.However,for the donation to be approved you have to send us proper documents that show you are in that crisis so as to avoid conning People.Please do email us your documents for approval.")
+                .and()
+                .addHtml(
+                    "If you have any questions, feel free to <a href=\"mailto:gofundme@gfm.com\">email our customer success team</a>. (We're lightning quick at replying.) We also offer <a href=\"{{live_chat_url}}\">live chat</a> during business hours."
+                ).and()
+                .addText(
+                    """
+        Cheers,
+        The GoFundMe Team
+        """.trimIndent()
+                ).and()
+                .copyright("rocketbase").url("https://www.rocketbase.io")
+                .and()
+                .addPlainTextFooter(
+                    """
+        [GoFundMe, LLC]
+        1234 Street Rd.
+        Suite 1234
+        """.trimIndent()
+                ).and()
+                .addImage("https://cdn.rocketbase.io/assets/loading/no-image.jpg","",100,0)
+                .linkUrl("https://www.rocketbase.io").and()
+                .build()
+
+            createAndSendApprovalEmail(htmlTextEmail,donationsModel)
+
+    }
+    fun createAndSendApprovalEmail(htmlTextEmail: HtmlTextEmail,donationsModel: DonationsModel){
+        val message=javaMailSender.createMimeMessage()
+        val messageHelper = MimeMessageHelper(message,MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,StandardCharsets.UTF_8.name())
+        messageHelper.setTo(donationsModel.createdBy.email)
+        messageHelper.setSubject("Donation Request Approval")
+        messageHelper.setText(htmlTextEmail.text,htmlTextEmail.html)
+        messageHelper.setFrom("gofundMeTeam@gfm.com")
+
+        try {
+            javaMailSender.send(message)
+        }
+        catch (e:MailException){
+            logStream.sendToLogConsole(LogStreamResponse(level ="ERROR",serviceAffected = "EmailService",message = "$e" ))
+        }
+
+
     }
 }
 
