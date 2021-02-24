559962730497fef3fdd5973296486345788a87d1
joswambugu <josphatwambugu77@gmail.com>
2021-02-24 08:15:52 +0300

    Integration Tests

--- src/test/kotlin/com/gofundme/server/ServerApplicationTests.kt
+++ src/test/kotlin/com/gofundme/server/ServerApplicationTests.kt
@@ -1,9 +1,16 @@
 package com.gofundme.server
 
 import org.junit.jupiter.api.Test
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestPropertySource
 
-@SpringBootTest
+@SpringBootTest(
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+    classes = [ServerApplication::class]
+)
+@AutoConfigureMockMvc
+@TestPropertySource(locations = ["classpath:application-test.properties"])
 class ServerApplicationTests {
 
     @Test
