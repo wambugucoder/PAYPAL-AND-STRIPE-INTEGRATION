08c9250953a40748283bdbce85175d43c3f757fe
joswambugu <josphatwambugu77@gmail.com>
2021-02-24 09:44:30 +0300

    Integration Tests

--- src/test/kotlin/com/gofundme/server/ServerApplicationTests.kt
+++ src/test/kotlin/com/gofundme/server/ServerApplicationTests.kt
@@ -1,18 +1,23 @@
 package com.gofundme.server
 
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.MethodOrderer
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestMethodOrder
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.gofundme.server.model.AddressModel
+import com.gofundme.server.model.UserModel
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.condition.EnabledOnJre
+import org.junit.jupiter.api.condition.JRE
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
 @SpringBootTest(
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
@@ -29,8 +34,30 @@ class ServerApplicationTests {
     @Autowired
     lateinit var mockMvc: MockMvc
 
+    @Autowired
+    lateinit var objectMapper: ObjectMapper
+
     @Test
-    fun contextLoads() {
+    @Order(1)
+    @DisplayName("/api/v1/auth/register")
+    @EnabledOnJre(JRE.JAVA_8,disabledReason = "Server was programmed to run on Java 8")
+    fun registerUser(){
+
+        // GIVEN
+        val addressDetails=AddressModel(country = "Kenya",city = "Nairobi")
+        val userDetails=UserModel(username = "abcdef",email = "abc@gmail.com",password = "123456",address = addressDetails)
+        val jsonBody=objectMapper.writeValueAsString(userDetails)
+
+        // WHEN
+        val finalResults=mockMvc.perform(
+            MockMvcRequestBuilders.post("/api/v1/auth/register").secure(true).content(jsonBody).contentType(MediaType.APPLICATION_JSON).accept(
+                MediaType.APPLICATION_JSON)
+        )
+        // EXPECTATIONS
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.message").exists())
+            //.andReturn()
+
     }
 
 }
