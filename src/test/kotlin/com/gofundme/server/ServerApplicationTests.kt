e6661c6bcb8370f6df426caea1e59eaa896c6f85
joswambugu <josphatwambugu77@gmail.com>
2021-02-24 08:23:43 +0300

    Integration Tests

--- src/test/kotlin/com/gofundme/server/ServerApplicationTests.kt
+++ src/test/kotlin/com/gofundme/server/ServerApplicationTests.kt
@@ -1,9 +1,18 @@
 package com.gofundme.server
 
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.MethodOrderer
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.TestPropertySource
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
 
 @SpringBootTest(
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
@@ -11,7 +20,14 @@ import org.springframework.test.context.TestPropertySource
 )
 @AutoConfigureMockMvc
 @TestPropertySource(locations = ["classpath:application-test.properties"])
+@ExtendWith(SpringExtension::class)
+@ActiveProfiles("test")
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+@DisplayName("GO-FUND-ME INTEGRATION TESTS")
 class ServerApplicationTests {
+
+    @Autowired
+    lateinit var mockMvc: MockMvc
 
     @Test
     fun contextLoads() {
