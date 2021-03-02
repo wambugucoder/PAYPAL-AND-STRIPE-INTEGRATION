98ffd8ffb537d4ad051328e972ecfaec9b91268b
joswambugu <josphatwambugu77@gmail.com>
2021-03-02 10:44:54 +0300

    Stripe integration Tests

--- src/test/kotlin/com/gofundme/server/ServerApplicationTests.kt
+++ src/test/kotlin/com/gofundme/server/ServerApplicationTests.kt
@@ -480,28 +480,6 @@ class ServerApplicationTests {
     }
     @Test
     @Order(22)
-    @DisplayName("/api/v1/users/{uid}/delete-donation/{did} -Expect 200")
-    @EnabledOnJre(JRE.JAVA_8,disabledReason = "Server was programmed to run on Java 8")
-    fun deleteDonation(){
-        // GIVEN A TOKEN,USER ID,DONATION ID
-        val userDetails=userRepository.findByEmail("abc@gmail.com")
-        val jwtToken=jwtService.generateLoginToken(userDetails)
-        val donationDetails=donationsRepository.findDonationByDetails("sales")
-
-        // PERFORM..
-        mockMvc.perform(
-            MockMvcRequestBuilders.delete("/api/v1/users/${userDetails.id}/delete-donation/${donationDetails.id}")
-                .secure(true)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .accept(MediaType.APPLICATION_JSON)
-                .header("Authorization","Bearer $jwtToken")
-        )
-            .andExpect(MockMvcResultMatchers.status().isOk)
-            .andReturn()
-
-    }
-    @Test
-    @Order(23)
     @DisplayName("/api/v1/admin/block-user/{email} -Expect 200")
     @EnabledOnJre(JRE.JAVA_8,disabledReason = "Server was programmed to run on Java 8")
     fun blockUser(){
